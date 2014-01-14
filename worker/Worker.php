bca799ce5d14bd1adf860a6e6cae624c94fd9c76
BenMatheja <bennimatheja@online.de>
2014-01-14 01:35:16 +0100

    worker adapted to cronjob

--- worker/Worker.php
+++ worker/Worker.php
@@ -15,92 +15,50 @@ require_once '../models/Jobs.php';
 
 /**
  * Init the Worker
+ * build to cron
  */
 class Worker
 {
     private $request_counter = 0;
-    private $name;
-    private $link;
     private $api_endpoint;
     private $rp;
     private $request;
-    private $jobs;
-    private $uid;
-    private $worker;
 
-    public function __construct($name)
+    public function __construct()
     {
-        $this->link = Database::Get();
-        $this->name = $name;
         $this->api_endpoint = new ApiEndpoint();
         $this->rp = new ResponseProcessor();
         $this->request = new Request($this->api_endpoint);
-        $this->uid = uniqid();
-        $this->persistWorker($this->name, $this->uid);
         $this->run();
-    }
-
-    private function persistWorker($name, $uid)
-    {
-        $new_worker = ORM::for_table('worker')->create();
-        $new_worker->name = $name;
-        $new_worker->worker_id = $uid;
-        $new_worker->requests_refreshed = date('Y-m-d H:i:s');
-        $new_worker->save();
-        $this->worker = $new_worker;
     }
 
     private function run()
     {
-        //change to while after finishing work
-        while (true) {
-           if ($this->requestsLeft() > 0) {
-                $this->loadJob();
-                if ($this->job) {
-                    $service_name = $this->job->serviceForJob();
-                    $strategy = $this->job->strategy;
-                    $response = $this->request->performRequest($this->request->buildRequest($service_name, $this->job->param));
-                    if ($this->rp->$strategy($response, $this->job->summoner_id)) {
-
-                        $this->job->fulfilled = 1;
-                        $this->job->fulfilled_at = date('Y-m-d H:i:s');
-                        $this->request_counter++;
-                        $this->job->save();
-
-                        $this->worker->requests_executed = $this->request_counter;
-                        $this->worker->last_request = date('Y-m-d H:i:s');
-                        $this->worker->requests_left = $this->requestsLeft() -1;
-                        $this->worker->save();
-                    }
+        $this->loadJobs();
+        if ($this->jobs) {
+            foreach ($this->jobs as $job) {
+                $service_name = $job->serviceForJob();
+                $strategy = $job->strategy;
+                $response = $this->request->performRequest($this->request->buildRequest($service_name, $job->param));
+                if ($this->rp->$strategy($response, $job->summoner_id)) {
+                    $job->fulfilled = 1;
+                    $job->fulfilled_at = date('Y-m-d H:i:s');
+                    $this->request_counter++;
+                    $job->save();
                 }
             }
         }
     }
 
-    private function getRunState()
+    private function loadJobs()
     {
-        $runstate = ORM::for_table('worker')->select('run')->where('worker_id', $this->uid)->find_one()->run;
-        if($runstate == 1) return true;
-            else return false;
-
-    }
-
-
-    private function requestsLeft()
-    {
-        $requests_left = ORM::for_table('worker')->where('worker_id', $this->uid)->find_one()->requests_left;
-        return $requests_left;
-    }
-
-    private function loadJob()
-    {
-        $job = Model::factory('Jobs')->order_by_asc('inserted')->limit(1)->where('fulfilled', 0)->find_one();
-        $this->job = $job;
+        $jobs = Model::factory('Jobs')->order_by_asc('inserted')->limit(1)->where('fulfilled', 0)->limit(5)->find_many();
+        $this->jobs = $jobs;
     }
 
 }
 
-$w = new Worker('machine spawned');
+$w = new Worker();
 // $data = $rp->process($request->performRequest($request->buildRequest('SummonerIdByName', 'dwaynehart')));
 /*
 array (size=2)
