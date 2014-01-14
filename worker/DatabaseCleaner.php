2787ec1ba4bff47f3871d46365ba2a43df0eba82
BenMatheja <bennimatheja@online.de>
2014-01-14 14:02:04 +0100

    DatabaseCleaner added

---
+++ worker/DatabaseCleaner.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ben
+ * Date: 14/01/14
+ * Time: 13:59
+ */
+require_once 'Config/Config.php';
+class DatabaseCleaner{
+
+    public function __construct(){
+
+        $this->cleanJobs();
+    }
+
+    private function cleanJobs(){
+        $query = ORM::raw_execute('DELETE FROM jobs where fulfilled = 1');
+    }
+
+
+}
+
+$dbc = new DatabaseCleaner();
--- worker/Disponent.php
+++ worker/Disponent.php
@@ -9,44 +9,58 @@
  * Friendly Job Generator
  */
 require_once 'Config/Config.php';
-class Disponent{
 
-    public function __construct(){
-    $this->autoloadModels();
-    $this->run();
+class Disponent
+{
+
+    public function __construct()
+    {
+        $this->autoloadModels();
+        $this->run();
     }
 
     private function autoloadModels()
     {
-        $model_array = array_diff( scandir(DIR_BASE.'../models'), array(".", "..",'.gitignore') );
+        $model_array = array_diff(scandir(DIR_BASE . '../models'), array(".", "..", '.gitignore'));
         foreach ($model_array as $model) {
-            require_once DIR_BASE.'../models/' . $model;
+            require_once DIR_BASE . '../models/' . $model;
         }
     }
 
-    public function CreateJobsForSummonerProfiles(){
+    public function run()
+    {
+        $this->CreateJobsForSummonerProfiles();
+    }
+
+    public function CreateJobsForSummonerProfiles()
+    {
         $summoner = Model::Factory('Summoner')->find_array();
-        foreach($summoner as $sum){
+        foreach ($summoner as $sum) {
             $new_job = Model::Factory('Jobs')->create();
-            if($sum['riot_id'] != null){
-               //create recentgames job
-               $new_job->services_id =  ORM::for_table('services')->where('name','RecentGames')->find_one()->id;
-               $new_job->strategy = ORM::for_table('services')->where('name','RecentGames')->find_one()->strategy;
-               $new_job->param = $sum['riot_id'];
-               $new_job->summoner_id = $sum['id'];
-               $new_job->save();
-               //create summoner update job
-               $new2_job = Model::Factory('Jobs')->create();
-               $new2_job->services_id =  ORM::for_table('services')->where('name','SummonerNameById')->find_one()->id;
-               $new2_job->strategy = ORM::for_table('services')->where('name','SummonerNameById')->find_one()->strategy;
-               $new2_job->param = $sum['riot_id'];
-               $new2_job->summoner_id = $sum['id'];
-               $new2_job->save();
+            if ($sum['riot_id'] != null) {
+                //only create jobs for summoners who are users
+                if ($sum['is_user'] == 1) {
+                    //create recentgames job
+                    $new_job->services_id = ORM::for_table('services')->where('name', 'RecentGames')->find_one()->id;
+                    $new_job->strategy = ORM::for_table('services')->where('name', 'RecentGames')->find_one()->strategy;
+                    $new_job->param = $sum['riot_id'];
+                    $new_job->summoner_id = $sum['id'];
+                    $new_job->save();
+                }
+                //create summoner update job
+                $new2_job = Model::Factory('Jobs')->create();
+                $new2_job->services_id = ORM::for_table('services')->where('name', 'SummonerNameById')->find_one()->id;
+                $new2_job->strategy = ORM::for_table('services')->where('name', 'SummonerNameById')->find_one()->strategy;
+                $new2_job->param = $sum['riot_id'];
+                $new2_job->summoner_id = $sum['id'];
+                $new2_job->save();
+
 
             }
-            if($sum['name'] != null && $sum['riot_id'] == null){
-            $new_job = Model::Factory('Jobs')->create();
-                $service = ORM::for_table('services')->where('name','SummonerIdByName')->find_one();
+            //Resolve names to summoner ids
+            if ($sum['name'] != null && $sum['riot_id'] == null) {
+                $new_job = Model::Factory('Jobs')->create();
+                $service = ORM::for_table('services')->where('name', 'SummonerIdByName')->find_one();
                 $new_job->services_id = $service->id;
                 $new_job->strategy = $service->strategy;
                 $new_job->param = $sum['name'];
@@ -54,11 +68,6 @@ class Disponent{
                 $new_job->save();
             }
         }
-    }
-
-
-    public function run(){
-         $this->CreateJobsForSummonerProfiles();
     }
 }
 
